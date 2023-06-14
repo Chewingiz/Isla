9680b61eb915991c469edf35de822454160e3515
Manal <andnothingevrything@gmail.com>
2023-06-14 13:06:42 +0200

    added landing animation

--- characters/character.gd
+++ characters/character.gd
@@ -1,46 +1,64 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@onready var animated_sprite = $AnimatedSprite2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animation_timer: Timer = $Timer
 
+var is_jumping = false
 
+func _ready():
+	animation_timer.wait_time = 0.2
+	animation_timer.connect("timeout",_on_animation_timer_timeout,0)
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		#animated_sprite.play("jump")
-		if velocity.y >=0:
-			animated_sprite.play("idle")# for fall("fall")
-		else:
-			animated_sprite.play("jump")
-	else:
-		if(velocity.x == 0):
-			animated_sprite.play("idle")
-		else:
-			animated_sprite.play("run")
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if animation_timer.is_stopped():
+		# Logique de mouvement et d'animation...
+		if not is_on_floor():
+			velocity.y += gravity * delta
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-		if velocity.x >0:
-			animated_sprite.flip_h = true
-		elif velocity.x<0:
-			animated_sprite.flip_h = false
-		
-		animated_sprite
+			if velocity.y >= 0:
+				if Input.is_action_pressed("ui_down"):
+					animated_sprite.play("recep")#anim chute rapide
+				else:
+					animated_sprite.play("idle")
+			else:
+				animated_sprite.play("jump")
+		else:
+			if is_jumping and Input.is_action_pressed("ui_down") :
+				is_jumping = false
+				animated_sprite.play("recep")
+				animation_timer.start()
+			else:
+				if velocity.x == 0:
+					animated_sprite.play("idle")
+				else:
+					animated_sprite.play("run")
+			
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			is_jumping = true
+
+		var direction = Input.get_axis("ui_left", "ui_right")
+		if direction:
+			velocity.x = direction * SPEED
+
+			if velocity.x > 0:
+				animated_sprite.flip_h = true
+			elif velocity.x < 0:
+				animated_sprite.flip_h = false
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if animated_sprite.animation == "recep":
+			velocity.x = 0
+			animated_sprite.stop()
+			#animated_sprite.play("idle")  # Jouez l'animation "idle" ou l'animation appropriée après les 2 secondes
+			is_jumping = false
 
 	move_and_slide()
-	#move_and_collide()
+
+func _on_animation_timer_timeout():
+	animation_timer.stop()
